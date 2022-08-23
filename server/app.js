3780c2cc9ef4c628bf6e4ea88feb3530c7e58756
Aaya-Elsharief <aaya.elsh@gmail.com>
2022-08-23 23:56:35 +0300

    add more structure related to #9

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const app = express();
-require('dotenv').config();
+require('env2').config();
 
 app.set('port', process.env.PORT || 8000);
 app.get('/', (req, res) => {
-    res.send('Hiiiii');
+  res.send('Hiiiii');
 });
 
 module.exports = app;
