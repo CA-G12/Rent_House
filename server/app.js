1248f706a328ce0790b071a9e47b85fd4c2a79b8
mohammadalwadia@gmail.com <54046997+mwadia@users.noreply.github.com>
2022-08-23 16:06:27 +0300

    create file structure Relates #2

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+require('dotenv').config();
+
+app.set('port', process.env.PORT || 8000);
+app.get('/', (req, res) => {
+    res.send('Hiiiii');
+});
+
+module.exports = app;
